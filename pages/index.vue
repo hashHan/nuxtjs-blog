3440b6134a921c8ee160b9ab80da0aee806c2b49
hashHan <haeseong.han@gmail.com>
2018-04-01 16:00:44 +0900

    Add menu and postlist

--- pages/index.vue
+++ pages/index.vue
@@ -3,32 +3,16 @@
     <section class="intro">
       <h1>Get the latest tech news!</h1>
     </section>
-    <section class="featured-posts">
-      <PostPreview
-        id="1"
-        thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-        title="Hello there!"
-        previewText="This my first post!" />
-      <PostPreview
-        id="2"
-        thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-        title="Hello there - the second time!"
-        previewText="This my second post!" />
-      <PostPreview
-        id="3"
-        thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-        title="Hi!"
-        previewText="This my third post!" />
-    </section>
+    <PostList />
   </div>
 </template>
 
 <script>
-import PostPreview from '@/components/Posts/PostPreview'
+import PostList from '@/components/Posts/PostList'
 
 export default {
   components: {
-    PostPreview
+    PostList
   }
 }
 </script>
@@ -40,6 +24,7 @@ export default {
   position: relative;
   padding: 30px;
   box-sizing: border-box;
+  background-image: url('~assets/images/main-page-background.jpg');
   background-position: center;
   background-size: cover;
 }
