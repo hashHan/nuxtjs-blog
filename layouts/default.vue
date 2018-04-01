f40d88b40b452d5a8f1c3f64b325e5efb041fdd4
hashHan <haeseong.han@gmail.com>
2018-04-01 15:49:53 +0900

    post component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,15 @@
 <template>
   <div>
-    <Header />
     <nuxt/>
   </div>
 </template>
 
-<script>
-import Header from '@/components/Header'
-
-export default {
-  components: {
-    Header
-  }
-}
-</script>
-
-
 <style>
+html {
+  font-family: 'Open Sans', sans-serif;
+}
 
+body {
+  margin: 0;
+}
 </style>
