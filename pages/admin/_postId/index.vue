e8c3f669e71d13ae9bd6430fbe663f0084a54be6
hashHan <haeseong.han@gmail.com>
2018-04-01 16:20:15 +0900

    signup login ui setting

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -10,14 +10,15 @@
 import AdminPostForm from '@/components/Admin/AdminPostForm'
 
 export default {
+  layout: 'admin',
   components: {
     AdminPostForm
   },
   data() {
     return {
       loadedPost: {
-        author: 'HAN',
-        title: 'My Post',
+        author: 'han',
+        title: 'My awesome Post',
         content: 'Super amazing, thanks for that!',
         thumbnailLink: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
       }
